4d3fb6d17c4b4ebce71b2ed1e67158ac8d35c363
Yoltic2 <eyolticmg@icloud.com>
2022-08-31 22:37:54 -0500

    Initial Commit

---
+++ Dictionary_game/Dictionary_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dictionary_gameApp.swift
+//  Dictionary_game
+//
+//  Created by Emilio Y Martinez on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Dictionary_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
